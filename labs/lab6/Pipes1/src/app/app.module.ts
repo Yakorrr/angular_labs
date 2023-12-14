b73a4951a328ca4f843f762b05d91379e66b8fb6
Yakorrr <vovkayakimenko2564@gmail.com>
2023-12-15 00:33:49 +0200

    Lab6 started, first task (basic pipes) in progress

---
+++ labs/lab6/Pipes1/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {FormatPipe} from './format.pipe';
+import {JoinPipe} from './join.pipe';
+
+@NgModule({
+    imports: [BrowserModule],
+    declarations: [AppComponent, FormatPipe, JoinPipe],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
