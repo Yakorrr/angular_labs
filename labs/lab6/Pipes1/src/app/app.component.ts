97b173aed0c3019657bd54420af42a2c10de4948
Yakorrr <vovkayakimenko2564@gmail.com>
2023-12-16 11:38:13 +0200

    Some fixes to Pipes1

--- labs/lab6/Pipes1/src/app/app.component.ts
+++ labs/lab6/Pipes1/src/app/app.component.ts
@@ -5,26 +5,41 @@ import {Component} from '@angular/core';
     template: `
         <div>Without formatting: {{myDate}}</div>
         <div>With formatting: {{myDate | date}}</div>
+        <hr>
+
         <div>{{welcome | uppercase}}</div>
         <div>{{welcome | lowercase}}</div>
         <div>{{percentage | percent}}</div>
         <div>{{percentage | currency}}</div>
+        <hr>
+
         <div>{{welcome | slice:3}}</div>
         <div>{{welcome | slice:6:11}}</div>
+        <hr>
+
+        <div>{{myNewDate | date:"dd/MM/yyyy"}}</div>
+        <hr>
+
         <div>{{pi | number:'2.1-2'}}</div>
         <div>{{pi | number:'3.5-5'}}</div>
+        <hr>
+
         <div>{{money | currency:'UAH':'code'}}</div>
         <div>{{money | currency:'UAH':'symbol-narrow'}}</div>
         <div>{{money | currency:'UAH':'symbol':'1.1-1'}}</div>
         <div>{{money | currency:'UAH':'symbol-narrow':'1.1-1'}}</div>
         <div>{{money | currency:'UAH':'Only today for '}}</div>
+        <hr>
+
         <div>{{message | slice:6:11 | uppercase}}</div>
+        <hr>
+
         <div>
             Number before formatting: {{x}}
             <br>
             Number after formatting: {{x | format}}
         </div>
-        <hr/>
+        <hr>
 
         <div>{{users | join}}</div>
         <div>{{users | join:1}}</div>
@@ -39,6 +54,7 @@ export class AppComponent {
     myDate = new Date(1961, 3, 12);
     welcome: string = "Hello World!";
     percentage: number = 0.14;
+    myNewDate = Date.now();
     pi: number = 3.1415;
     money: number = 23.45;
     message = "Hello World!";
