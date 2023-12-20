8bb2342d45b9ba6f76484af49cd3abaa3d28f375
Yakorrr <vovkayakimenko2564@gmail.com>
2023-12-20 18:32:58 +0200

    1. Created and added routing configuration. 2. Navigation in project implemented. 3. Some route guards added. 4. Cart fully created. 5. Basic order placement created, some info about order processed. 6. Repository and data source updated. 7. Some changes to project done

--- labs/lab8/SportsShop/src/app/store/store.module.ts
+++ labs/lab8/SportsShop/src/app/store/store.module.ts
@@ -4,12 +4,13 @@ import {FormsModule} from "@angular/forms";
 import {ModelModule} from "../model/model.module";
 import {StoreComponent} from "./store.component";
 import {CounterDirective} from "./counter.directive";
-import {CartSummaryComponent} from "./cartsummary.component";
+import {CartSummaryComponent} from "./cartSummary.component";
 import {CartDetailComponent} from "./cartDetail.component";
 import {CheckoutComponent} from "./checkout.component";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
-    imports: [ModelModule, BrowserModule, FormsModule],
+    imports: [ModelModule, BrowserModule, FormsModule, RouterModule],
     declarations: [StoreComponent, CounterDirective, CartSummaryComponent,
         CartDetailComponent, CheckoutComponent],
     exports: [StoreComponent, CartDetailComponent, CheckoutComponent]
