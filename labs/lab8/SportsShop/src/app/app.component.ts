fbd026137cc7055279153a5ddeb25147ec72854a
Yakorrr <vovkayakimenko2564@gmail.com>
2023-12-20 12:25:19 +0200

    1. Data for json-server created (data.js) 2. Basic auth implemented (authMiddleware.js) 3. Created basic model for project 4. Created basic component and template for shop 5. Some changes to project

--- labs/lab8/SportsShop/src/app/app.component.ts
+++ labs/lab8/SportsShop/src/app/app.component.ts
@@ -1,18 +1,12 @@
 import {Component} from '@angular/core';
-import {CommonModule} from '@angular/common';
 
 @Component({
-  selector: 'app',
-  // standalone: true,
-  // imports: [CommonModule],
-  // templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  template: `
-    <div class="bg-success p-2 text-center text-white">
-      This is the SportShop
-    </div>
-  `
+    selector: 'app',
+    template: `
+        <store></store>
+    `
 })
+
 export class AppComponent {
-  title = 'SportsShop';
+    title = 'SportsShop';
 }
